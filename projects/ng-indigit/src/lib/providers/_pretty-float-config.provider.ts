545548b94de4dbf6d1424620567538976d536bea
VahidM <v.mohagheghi@asax.local>
2022-08-02 16:03:15 +0430

    fix(core): "value" passed as "NG_INDIGIT_PRETTY_FLOAT_CONFIG" is type-safe

--- projects/ng-indigit/src/lib/providers/_pretty-float-config.provider.ts
+++ projects/ng-indigit/src/lib/providers/_pretty-float-config.provider.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from '@angular/core';
 import { IPrettyFloatOption } from '../interfaces';
 
-export const NG_INDIGIT_PRETTY_FLOAT_CONFIG
+export const NG_INDIGIT_PRETTY_FLOAT_CONFIG: InjectionToken<IPrettyFloatOption>
   = new InjectionToken<IPrettyFloatOption>('DefaultPrettyFloatConfiguration');
