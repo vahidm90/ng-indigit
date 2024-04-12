0e38e939d3c5bd7bde3c302690f300aebd680ff2
Hossein Salmanian <Salmanian.Hossein@gmail.com>
2024-04-12 16:46:42 +0330

    chore: update to Angular 17 BREAKING CHANGE: dependencies updated to Angular 17

--- projects/ng-indigit-demo/src/app/app.component.spec.ts
+++ projects/ng-indigit-demo/src/app/app.component.spec.ts
@@ -16,12 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-indigit-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-indigit-demo');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
